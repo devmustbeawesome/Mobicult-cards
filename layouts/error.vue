c29a2c9c0c09c98384b82635f1c39ea8d1870285
devmustbeawesome <programm.rechner@gmail.com>
2024-03-01 12:06:03 +0400

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+}
+</script>
+<template>
+    <div class="error_block">
+        <h1 class="error_title" v-if="error.statusCode === 404">Page not found</h1>
+        <h1 class="error_title" v-else>An error occurred - {{ error.statusCode }} </h1>
+        <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+</template>
+<style>
+.error_block {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+
